c14e04a19ed7c00b595d49c96454743deec64409
AadyaS26 <aadya.syal@students.asdrp.org>
2026-07-19 00:51:02 -0700

    Update Impact.jsx

--- src/pages/Impact.jsx
+++ src/pages/Impact.jsx
@@ -7,7 +7,7 @@ const FONT_IMPORT = `@import url('https://fonts.googleapis.com/css2?family=Fraun
 
 const CURATED_BILLS = 212;
 const CONDITIONS_COVERED = 146;
-const INSTAGRAM_REACH = 828;
+const INSTAGRAM_REACH = 829;
 const COUNTRIES_REACHED = 22;
 
 const RECENTLY_PASSED = [
