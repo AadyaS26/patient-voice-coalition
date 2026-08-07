d744fe9fc5f5d70400417205463e922cd74da4ef
AadyaS26 <aadya.syal@students.asdrp.org>
2026-08-06 17:07:25 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
 import Legislation from "./pages/Legislation.jsx";
-import FindRepresentatives from "./pages/FindRepresentatives";
 import Impact from "./pages/Impact.jsx";
 import CreateChapter from './pages/CreateChapter';
 import Events from "./pages/Events.jsx";
@@ -39,7 +38,6 @@ export default function App() {
         <Route path="/resource-library" element={<ResourceLibrary />} />
         <Route path="/legislation" element={<Legislation />} />
         <Route path="/impact" element={<Impact />} />
-        <Route path="/find-representatives" element={<FindRepresentatives />} />
         <Route path="/events" element={<Events />} />
         <Route path="/brainstorm" element={<Brainstorm />} />
         <Route path="/create-chapter" element={<CreateChapter />} />
